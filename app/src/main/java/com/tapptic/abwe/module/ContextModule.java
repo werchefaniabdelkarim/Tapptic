7aaf2fa36836599d008a91fe4c8679caf4d6d9f6
werchefaniabdelkarim <31032792+werchefaniabdelkarim@users.noreply.github.com>
2018-10-17 11:07:00 +0200

    Update app/src/main/java/com/tapptic/abwe/module/ContextModule.java

---
+++ app/src/main/java/com/tapptic/abwe/module/ContextModule.java
@@ -0,0 +1,21 @@
+package com.tapptic.abwe.module;
+
+import android.content.Context;
+
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class ContextModule {
+    private Context context;
+
+    public ContextModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+}
