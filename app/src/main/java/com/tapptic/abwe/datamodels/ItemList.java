e7c6bf23ea5d5461a8dc8cbee2bc7dc8d4c87d98
werchefaniabdelkarim <31032792+werchefaniabdelkarim@users.noreply.github.com>
2018-10-17 11:07:21 +0200

    Update app/src/main/java/com/tapptic/abwe/datamodels/ItemList.java

---
+++ app/src/main/java/com/tapptic/abwe/datamodels/ItemList.java
@@ -0,0 +1,11 @@
+package com.tapptic.abwe.datamodels;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ItemList {
+
+    @SerializedName("name")
+    public String name;
+    @SerializedName("image")
+    public String image;
+}
